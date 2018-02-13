8a9c631d46ea3fab3f5d9a3187fc2021125761c5
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-02-13 15:18:37 -0800

    added basic client side w/ event handler - bug that throws 400 error

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,78 @@
+//this is where all the client code goes
+
+//client needs to take a user input with the sales report example as an object
+//flatten that object and be sent to the server
+//rendered on the DOM as text and has a CSV report
+//the input value will be a tree data structure
+//and will have a "children" property
+
+//we need to flatten the tree object:
+//flatten will take in the user input
+// var flatten = (input) => {
+//   var
+//   if (typeof input === 'object') {
+//
+//   }
+// };
+// var value = document.getElementById('inputs').value;
+// console.log(value);
+var app = {};
+var value;
+//store the user input from the DOM
+// $('form').on('submit', function(e){
+//   e.preventDefault();
+// });
+
+// app.userInput = () => {
+//   // let value = document.getElementById('inputs').innerHTML.value;
+//   // let value = $('#inputs').val();
+//   value = $('#inputs').val();
+//   $('#inputs').val('');
+//   console.log(value);
+// };
+
+$('button').on('click', (e) => {
+  e.preventDefault();
+  app.handleSubmit();
+});
 
 
+app.handleSubmit = () => {
+  let userInput = $('#inputs').val();
+  console.log(userInput);
+  // $('button').on('click', (e) => {
+  //   e.preventDefault();
+    // app.userInput();
+    app.send(userInput);
+    console.log('i was clicked', userInput);
+    $('#inputs').val('');
+  // });
+  // $('#inputs').val('');
+  // app.send(userInput);
+  // $('#inputs').val('');
+};
+
+//app initialization
+app.init = () => {
+  // app.userInput();
+  // app.handleSubmit();
+  // app.send(value);
+  $('#text-box').on('submit', app.handleSubmit);
+}
+
+//AJAX request to send req to the server side
+app.send = (input) => {
+  console.log('i was invoked', input);
+  $.ajax({
+    type: 'POST',
+    url: '/',
+    data: input,
+    contentType: 'application/json',
+    success: (data) => {
+      console.log('I got data bro', data);
+    },
+    error: (data) => {
+      console.log("error", data);
+    }
+  });
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,34 @@
+const express = require('express');
+const parser = require('body-parser');
+// const router = require('./router.js');
+const app = express();
 
+module.exports.app = app;
 
+//port we're listening on
+app.set('port', 3000);
+
+//parsing
+app.use(parser.json());
+
+//setting the routes
+// app.get('/', (req, res) => {
+//   res.send();
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send();
+// });
+
+//server the client files
+app.use(express.static('client'));
+
+app.post('/', (req, res) => {
+  res.status(200);
+  res.send();
+})
+
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
