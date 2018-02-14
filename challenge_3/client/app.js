42095ffcc846ff161ff383aa86eb887094c750a2
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-02-13 22:57:44 -0800

    setup app.js, index, and server + dependencies ahead of sprint to save time

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,2 +1,9 @@
-
-
+class App extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return FILL_ME_IN
+  }
+}
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,27 @@
+const express = require(`express`);
+const parser = require('body-parser');
+const app = express();
 
+module.exports.app = app;
 
+app.set(`port`, 3000);
+
+//remember that semi-colons are not json
+app.use(parser.json());
+
+// app.use(express.static('client'));
+// app.get('/', (req, res, next) => {
+//   let data = req.body;
+//   res.status();
+//   res.send(`successful get!`);
+// });
+//
+// app.post('/', (req, res, next) => {
+//   res.status(200);
+//   res.send(`successful post!`)
+// });
+
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
