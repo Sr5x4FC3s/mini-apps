5e76569744ef443f6190fd6f77fcb3d938410716
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-02-13 19:33:49 -0800

    added almost everything, but incomplete because the recursive function in the server doesn't work correctly with the json parsed object

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,78 +1,44 @@
-//this is where all the client code goes
 
-//client needs to take a user input with the sales report example as an object
-//flatten that object and be sent to the server
-//rendered on the DOM as text and has a CSV report
-//the input value will be a tree data structure
-//and will have a "children" property
-
-//we need to flatten the tree object:
-//flatten will take in the user input
-// var flatten = (input) => {
-//   var
-//   if (typeof input === 'object') {
-//
-//   }
-// };
-// var value = document.getElementById('inputs').value;
-// console.log(value);
 var app = {};
 var value;
-//store the user input from the DOM
-// $('form').on('submit', function(e){
-//   e.preventDefault();
-// });
-
-// app.userInput = () => {
-//   // let value = document.getElementById('inputs').innerHTML.value;
-//   // let value = $('#inputs').val();
-//   value = $('#inputs').val();
-//   $('#inputs').val('');
-//   console.log(value);
-// };
 
 $('button').on('click', (e) => {
+  let userInput = $('#inputs').val();
   e.preventDefault();
-  app.handleSubmit();
+  app.handleSubmit(userInput);
 });
 
-
-app.handleSubmit = () => {
-  let userInput = $('#inputs').val();
-  console.log(userInput);
-  // $('button').on('click', (e) => {
-  //   e.preventDefault();
-    // app.userInput();
-    app.send(userInput);
-    console.log('i was clicked', userInput);
+app.handleSubmit = (input) => {
+    app.send(input);
     $('#inputs').val('');
-  // });
-  // $('#inputs').val('');
-  // app.send(userInput);
-  // $('#inputs').val('');
 };
 
 //app initialization
 app.init = () => {
-  // app.userInput();
-  // app.handleSubmit();
-  // app.send(value);
   $('#text-box').on('submit', app.handleSubmit);
 }
 
 //AJAX request to send req to the server side
 app.send = (input) => {
-  console.log('i was invoked', input);
   $.ajax({
     type: 'POST',
     url: '/',
     data: input,
+    // data: input,
     contentType: 'application/json',
+    // contentType: 'application/json',
     success: (data) => {
       console.log('I got data bro', data);
+      app.render(data);
     },
     error: (data) => {
       console.log("error", data);
     }
   });
+  // $.post('/', input, (res) => {
+  //   console.log('success', res);
+  // });
+  app.render = (array) => {
+    //map thru the passed array and  append each to a new div tag on the dom
+  }
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,31 +1,60 @@
-const express = require('express');
-const parser = require('body-parser');
+const express = require(`express`);
+const parser = require(`body-parser`);
+// const helper = require(`helper.js`);
 // const router = require('./router.js');
 const app = express();
+
+var values = [];
+var properties = (object) => {
+  var keys = Object.keys(object);
+  keys.pop();
+  return keys;
+};
+
+var values = [];
+
+var recurse = function(object) {
+  if (object !== undefined || null) {
+    values.push(Object.values(object));
+    values.pop();
+  }
+  if (object.children) {
+    for (var i = 0; i < object['children'].length; i++) {
+      recurse(object.children[i]);
+    }
+  }
+  return values;
+}
+
+var removeChildren = (array) => {
+  array.forEach(element => {
+    element.pop();
+  });
+  return array;
+}
 
 module.exports.app = app;
 
 //port we're listening on
-app.set('port', 3000);
+app.set(`port`, 3000);
 
 //parsing
 app.use(parser.json());
 
-//setting the routes
-// app.get('/', (req, res) => {
-//   res.send();
-// });
-//
-// app.post('/', (req, res) => {
-//   res.send();
-// });
-
 //server the client files
 app.use(express.static('client'));
 
-app.post('/', (req, res) => {
+app.post(`/`, (req, res) => {
+  let data = req.body;
+  console.log('data', data);
+  console.log('typeof:', typeof data);
+  var values = [];
+  var property = properties(data);
+  console.log(property);
+  recurse(data);
+  // removeChildren(values)
   res.status(200);
-  res.send();
+  res.send('success dawg');
 })
 
 if (!module.parent) {
